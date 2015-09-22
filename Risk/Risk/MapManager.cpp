7b4da21170a3c243dc43b63addce7bd8f5b6f53e
Spirielle <spirielle@hotmail.com>
2015-09-22 13:22:55 -0400

    Created the files for Map and MapManager classes

---
+++ Risk/Risk/MapManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "MapManager.h"
+
+
+MapManager::MapManager()
+{
+}
+
+
+MapManager::~MapManager()
+{
+}
---
+++ Risk/Risk/MapManager.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//Will be used to load and save maps
+//TODO: find a simple serialization library
+class MapManager
+{
+public:
+	MapManager();
+	~MapManager();
+};
+
